759a5eb52324d7305b4cb152c016bff75634a1a4
rubayedhasan <mdrubayedhasan2002@gmail.com>
2024-07-14 10:28:11 +0600

    write some line of code to update seat-indicator & selected indicator inner value

--- scripts/ticketing-app.js
+++ scripts/ticketing-app.js
@@ -1,1 +1,27 @@
-// alert("ticketing app connected");
+// collection of bus btn
+const btnCollection = document.getElementsByClassName("seat-btn");
+// iterate btnCollection
+for (const btn of btnCollection) {
+  btn.addEventListener("click", function (event) {
+    // get the seats
+    let countOfSeats = getInnerValueAsNumber("seat-indicator");
+
+    // reduce counting value
+    countOfSeats = countOfSeats - 1;
+    // stop updating after zero
+    if (countOfSeats < 0) {
+      alert("No seat available");
+      return;
+    }
+    // update seat-indicator inner value
+    setInnerValue("seat-indicator", countOfSeats);
+
+    // count selected seats
+    let selectedSeatCount = getInnerValueAsNumber("selected-seat-indicator");
+
+    // increase selected seat count
+    selectedSeatCount = selectedSeatCount + 1;
+    // update selected seat count
+    setInnerValue("selected-seat-indicator", selectedSeatCount);
+  });
+}
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,1 +1,19 @@
-// alert("utilities connected");
+// get a element's  inner text value (string type)
+function getInnerValue(elementID) {
+  const element = document.getElementById(elementID);
+  const innerValue = element.innerText;
+}
+
+// get a element's innerText value as Number (number type)
+function getInnerValueAsNumber(elementID) {
+  const element = document.getElementById(elementID);
+  const innerValue = element.innerText;
+  const value = parseInt(innerValue);
+  return value;
+}
+
+// set innerText value
+function setInnerValue(elementID, setValue) {
+  const element = document.getElementById(elementID);
+  element.innerText = setValue;
+}
